e1f3dac62bb210c9cd03dd2ee282bde7a58d2c1d
Nishali Naik <nishalinaik@airbears2-10-142-93-129.airbears2.1918.berkeley.edu>
2018-11-03 15:30:46 -0700

    Location Button

--- you-choose/client/main.js
+++ you-choose/client/main.js
@@ -9,6 +9,7 @@ Handlebars.registerHelper('toCapitalCase', function (str) {
 
 function initUserLanguage() {
   var language = amplify.store("language");
+
   setUserLanguage("en");
 }
 
@@ -18,6 +19,35 @@ function getCurrentGame(){
   if (gameID) {
     return Games.findOne(gameID);
   }
+}
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+    var img = new Image();
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+
+    output.appendChild(img);
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
 }
 
 function getAccessLink(){
@@ -132,6 +162,7 @@ function hasHistoryApi () {
   return !!(window.history && window.history.pushState);
 }
 
+
 Meteor.setInterval(function () {
   Session.set('time', new Date());
 }, 1000);
@@ -183,6 +214,7 @@ Template.main.helpers({
 Template.startMenu.events({
   'click #btn-new-game': function () {
     Session.set("currentView", "createGame");
+
   },
   'click #btn-join-game': function () {
     Session.set("currentView", "joinGame");
